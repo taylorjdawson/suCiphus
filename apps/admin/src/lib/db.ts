28614448b4c28dbcc6b3643382234939e16b3159
Taylor Dawson <taylorjdawson@gmail.com>
2024-05-31 16:20:51 -0700

    Update supabase to use ssr

--- apps/admin/src/lib/db.ts
+++ apps/admin/src/lib/db.ts
@@ -1,19 +1,31 @@
-import { createClient } from "@supabase/supabase-js"
+import { cookies } from "next/headers"
+import { createServerClient } from "@supabase/ssr"
 import { objectToCamel } from "ts-case-convert"
 
 import { env } from "@/env.mjs"
 import { Database } from "@/types/database.types"
 import { Thread } from "@/types/types"
 
-// Create a single supabase client for interacting with your database
-const supabase = createClient<Database>(
-  env.SUPABASE_URL,
-  env.SUPABASE_SERVICE_KEY
-)
+const getClient = () => {
+  const cookieStore = cookies()
+
+  return createServerClient<Database>(
+    env.SUPABASE_URL,
+    env.SUPABASE_SERVICE_KEY,
+    {
+      cookies: {
+        get(name: string) {
+          return cookieStore.get(name)?.value
+        },
+      },
+    }
+  )
+}
 
 export const newThread = async (
   thread: Database["public"]["Tables"]["threads"]["Insert"]
 ) => {
+  const supabase = getClient()
   const { data, error } = await supabase
     .from("threads")
     .insert([thread])
@@ -24,6 +36,7 @@ export const getThreads = async (
   threadId?: string
 ): Promise<Thread.Thread[]> => {
   try {
+    const supabase = getClient()
     const query = supabase.from("threads").select("*")
     if (threadId) {
       query.eq("id", threadId)
